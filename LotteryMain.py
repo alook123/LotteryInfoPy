70ed99b79b0694301270cdf45afdab7580b6b155
alook_6@live.com <alook_6@live.com>
2018-10-09 14:42:45 +0800

    添加配置文件以及设置路径功能

--- LotteryMain.py
+++ LotteryMain.py
@@ -5,11 +5,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets,uic
 from bs4 import BeautifulSoup
 from xlwt import *
 from common import get,getColorStyle
+from configparser import ConfigParser
+cfg = ConfigParser()
+cfg.read('config.ini')
+excelPath=cfg.get('path','excelPath')
 
- 
 qtCreatorFile = "untitled.ui" # Enter file here.
- 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
  
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -19,9 +23,18 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         #获取当前时间并赋值
         now_day = time.strftime("%Y-%m-%d", time.localtime())
         self.dateEdit.setDate(QtCore.QDate.fromString(now_day, 'yyyy-MM-dd'))
-        self.btnManualOpen.clicked.connect(self.CalculateTax)
+        self.btnSetDir.clicked.connect(self.SetDir)
+        self.btnManualOpen.clicked.connect(self.LookExcelByDate)
 
-    def CalculateTax(self):
+    def SetDir(self):
+        directory1 = QtWidgets.QFileDialog.getExistingDirectory(self,"选取文件夹","./") #起始路径
+        self.excelPath=directory1
+        cfg.set('path','excelPath',self.excelPath)
+        # write to file
+        with open("config.ini","w+") as f:
+            cfg.write(f)
+
+    def LookExcelByDate(self):
         #获取dateEdit的时间
         yyyy = self.dateEdit.sectionText(self.dateEdit.sectionAt(0))
         mm=self.dateEdit.sectionText(self.dateEdit.sectionAt(1))
@@ -30,27 +43,29 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
         book = Workbook() #创建一个Excel
         sheet1 = book.add_sheet('sheet1') #在其中创建一个sheet
-        sheet1.write(0,0,'期数')
-        sheet1.write(0,1,'时间')
-        sheet1.write(0,2,'号码1')
-        sheet1.write(0,3,'号码2')
-        sheet1.write(0,4,'号码3')
-        sheet1.write(0,5,'号码4')
-        sheet1.write(0,6,'号码5')
-        sheet1.write(0,7,'号码6')
-        sheet1.write(0,8,'号码7')
-        sheet1.write(0,9,'号码8')
-        sheet1.write(0,10,'号码9')
-        sheet1.write(0,11,'号码10')
+
+        sheet1.write_merge(0,2,0,11,'')
+
+        sheet1.write(3,0,'期数')
+        sheet1.write(3,1,'时间')
+        sheet1.write(3,2,'号码1')
+        sheet1.write(3,3,'号码2')
+        sheet1.write(3,4,'号码3')
+        sheet1.write(3,5,'号码4')
+        sheet1.write(3,6,'号码5')
+        sheet1.write(3,7,'号码6')
+        sheet1.write(3,8,'号码7')
+        sheet1.write(3,9,'号码8')
+        sheet1.write(3,10,'号码9')
+        sheet1.write(3,11,'号码10')
 
         url="https://www.1396j.com/pk10/kaijiang?date="+datestr
         soup=BeautifulSoup(get(url))
         for index,item in enumerate(soup.select('#history tbody tr')):
-            print(index)
             id= item.find("i", class_="font_gray666").string
-            sheet1.write(index+1,0,id)
+            sheet1.write(index+4,0,id)
             date= item.find("i", class_="font_gray999").string
-            sheet1.write(index+1,1,date)
+            sheet1.write(index+4,1,date)
             numstr=''
             listnum=[]
             count=0
@@ -59,11 +74,11 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 if item2.string.strip()!='':
                     listnum.append(item2.string.strip())
                     style = getColorStyle(int(item2.string.strip()))
-                    sheet1.write(index+1,count+2,item2.string.strip(),style=style)
+                    sheet1.write(index+4,count+2,item2.string.strip(),style=style)
                     count+=1
-            print(listnum)
-        book.save(datestr+'.xls')
-        os.startfile(datestr+'.xls')
+        book.save(self.excelPath+'/'+datestr+'.xls')
+        path = os.path.abspath(self.excelPath+'/'+datestr+'.xls')
+        os.startfile(path)
 
 if __name__ == "__main__":
     app =QtWidgets.QApplication(sys.argv)
